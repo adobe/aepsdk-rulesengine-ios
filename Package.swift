9925fe425e337297c3b88e442795cca5427cc56d
Jiabin Geng <gengjiabin@gmail.com>
2020-07-20 16:59:11 -0600

    update the package name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "RulesEngine",
+    name: "SwiftRulesEngine",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "RulesEngine",
+            name: "SwiftRulesEngine",
             targets: ["RulesEngine"]),
 //        
 //        .library(
