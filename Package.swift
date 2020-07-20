6b99704c496039bb2f85b0f180c3ef87d4166763
Jiabin Geng <gengjiabin@gmail.com>
2020-07-20 17:01:40 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftRulesEngine",
-            targets: ["RulesEngine"]),
+            targets: ["SwiftRulesEngine"]),
 //        
 //        .library(
 //            name: "RulesDSL",
@@ -24,11 +24,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "RulesEngine",
+            name: "SwiftRulesEngine",
             dependencies: []),
         .testTarget(
-            name: "RulesEngineTests",
-            dependencies: ["RulesEngine"]),
+            name: "SwiftRulesEngineTests",
+            dependencies: ["SwiftRulesEngine"]),
 //        .target(
 //            name: "RulesDSL",
 //            dependencies: []),
