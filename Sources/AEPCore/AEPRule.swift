03d32a9b475822399aca4cdf5bca873bd6e8024e
Yansong <yanyang@adobe.com>
2020-07-21 14:13:16 -0600

    WIP: present an idea for discussion

--- Sources/AEPCore/AEPRule.swift
+++ Sources/AEPCore/AEPRule.swift
@@ -16,6 +16,17 @@ import SwiftRulesEngine
 struct LaunchRuleRoot: Codable {
     var version: Int
     var rules: [LaunchRule]
+
+    func convert() -> [AEPRule] {
+        var result = [AEPRule]()
+        for launchRule in rules {
+            if let conditionExpression = launchRule.condition.convert() {
+                let rule = AEPRule(condition: conditionExpression)
+                result.append(rule)
+            }
+        }
+        return result
+    }
 }
 
 struct LaunchRule: Codable {
@@ -31,12 +42,72 @@ enum ConditionType: String, Codable {
 class Condition: Codable {
     var type: ConditionType
     var definition: Definition
+    func convert() -> Evaluable? {
+        switch type {
+        case .group:
+            if let operationStr = definition.logic, let subConditions = definition.conditions {
+                var operands = [Evaluable]()
+                for subCondition in subConditions {
+                    if let operand = subCondition.convert() {
+                        operands.append(operand)
+                    }
+                }
+                return operands.count == 0 ? nil : LogicalExpression(operationName: operationStr, operands: operands)
+            }
+            return nil
+        case .matcher:
+            if let key = definition.key, let matcher = definition.matcher, let values = definition.values {
+                if values.count == 1 {
+                    return convert(key: key, matcher: matcher, anyCodable: values[0])
+                }
+                if values.count > 1 {
+                    var operands = [Evaluable]()
+                    for value in values {
+                        if let operand = convert(key: key, matcher: matcher, anyCodable: value) {
+                            operands.append(operand)
+                        }
+                    }
+                    return operands.count == 0 ? nil : LogicalExpression(operationName: "or", operands: operands)
+                }
+            }
+            return nil
+        }
+    }
+
+    func convert(key: String, matcher: String, anyCodable: AnyCodable) -> Evaluable? {
+        if let value = anyCodable.value {
+            switch value {
+            case is String:
+                if let stringValue = anyCodable.stringValue {
+                    return ComparisonExpression<MustacheToken, String>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(stringLiteral: stringValue))
+                }
+                return nil
+            //            case is Double:
+            //                return ComparisonExpression<Any, Double>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(: anyCodable.doubleValue))
+            case is Int:
+                if let intValue = anyCodable.intValue {
+                    return ComparisonExpression<MustacheToken, Int>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(integerLiteral: intValue))
+                }
+                return nil
+            case is Float:
+                if let floadValue = anyCodable.floatValue {
+                    return ComparisonExpression<MustacheToken, Double>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(floatLiteral: Double(floadValue)))
+                }
+                return nil
+            //            case is Int64:
+            //                return ComparisonExpression(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(anyCodable.longValue))
+            default:
+                return nil
+            }
+        }
+        return nil
+    }
 }
 
 struct Definition: Codable {
     let logic: String?
     let conditions: [Condition]?
-    let key: AnyCodable?
+    let key: String?
     let matcher: String?
     let values: [AnyCodable]?
 }
@@ -71,38 +142,88 @@ class LaunchRulesParser {
 
 public class AEPRule: Rule {
     public let condition: Evaluable
-    private init(condition: Evaluable) {
+    fileprivate init(condition: Evaluable) {
         self.condition = condition
     }
 
     public static func generateRules(json: String) -> [AEPRule] {
         if let launchRulesRoot = LaunchRulesParser.parse(json) {
-            return convert(launchRulesRoot)
+            return launchRulesRoot.convert()
         } else {
             return []
         }
     }
 
-    private static func convert(_ root: LaunchRuleRoot) -> [AEPRule] {
-        var result = [AEPRule]()
-        for launchRule in root.rules {
-            if let conditionExpression = convert(launchRule.condition) {
-                let rule = AEPRule(condition: conditionExpression)
-                result.append(rule)
-            }
-        }
-        return result
-    }
-
-    private static func convert(_ condition: Condition) -> LogicalExpression? {
-       return nil
-    }
-
-//    private static func convert(_ condition: Condition) -> ComparisonExpression? {
-//        guard condition.type == .matcher else {
+//
+//    private static func convert(_ root: LaunchRuleRoot) -> [AEPRule] {
+//        var result = [AEPRule]()
+//        for launchRule in root.rules {
+//            if let conditionExpression = convert(launchRule.condition) {
+//                let rule = AEPRule(condition: conditionExpression)
+//                result.append(rule)
+//            }
+//        }
+//        return result
+//    }
+//
+//    private static func convert(_ condition: Condition) -> Evaluable? {
+//        switch condition.type {
+//        case .group:
+//            if let operationStr = condition.definition.logic, let subConditions = condition.definition.conditions {
+//                var operands = [Evaluable]()
+//                for subCondition in subConditions {
+//                    if let operand = convert(subCondition) {
+//                        operands.append(operand)
+//                    }
+//                }
+//                return operands.count == 0 ? nil : LogicalExpression(operationName: operationStr, operands: operands)
+//            }
+//            return nil
+//        case .matcher:
+//            if let key = condition.definition.key, let matcher = condition.definition.matcher, let values = condition.definition.values {
+//                if values.count == 1 {
+//                    return convert(key: key, matcher: matcher, anyCodable: values[0])
+//                }
+//                if values.count > 1 {
+//                    var operands = [Evaluable]()
+//                    for value in values {
+//                        if let operand = convert(key: key, matcher: matcher, anyCodable: value) {
+//                            operands.append(operand)
+//                        }
+//                    }
+//                    return operands.count == 0 ? nil : LogicalExpression(operationName: "or", operands: operands)
+//                }
+//            }
 //            return nil
 //        }
+//    }
 //
+//    private static func convert(key: String, matcher: String, anyCodable: AnyCodable) -> Evaluable? {
+//        if let value = anyCodable.value {
+//            switch value {
+//            case is String:
+//                if let stringValue = anyCodable.stringValue {
+//                    return ComparisonExpression<MustacheToken, String>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(stringLiteral: stringValue))
+//                }
+//                return nil
+    ////            case is Double:
+    ////                return ComparisonExpression<Any, Double>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(: anyCodable.doubleValue))
+//            case is Int:
+//                if let intValue = anyCodable.intValue {
+//                    return ComparisonExpression<MustacheToken, Int>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(integerLiteral: intValue))
+//                }
+//                return nil
+//            case is Float:
+//                if let floadValue = anyCodable.floatValue {
+//                    return ComparisonExpression<MustacheToken, Double>(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(floatLiteral: Double(floadValue)))
+//                }
+//                return nil
+    ////            case is Int64:
+    ////                return ComparisonExpression(lhs: Operand(mustache: key), operationName: matcher, rhs: Operand(anyCodable.longValue))
+//            default:
+//                return nil
+//            }
+//        }
 //        return nil
 //    }
 }
