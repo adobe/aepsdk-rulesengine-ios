7a73e6825366af07eedf35be28081a4c963022a6
Yansong <yanyang@adobe.com>
2020-10-05 10:27:55 -0500

    RulesEngineLoggingTests

---
+++ Tests/AEPRulesEngineTests/UnitTests/RulesEngineLoggingTests.swift
@@ -0,0 +1,42 @@
+/*
+ Copyright 2020 Adobe. All rights reserved.
+ This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ OF ANY KIND, either express or implied. See the License for the specific language
+ governing permissions and limitations under the License.
+ */
+
+import Foundation
+import XCTest
+
+@testable import AEPRulesEngine
+
+private class Log: RulesEngineLogging {
+    public var message = ""
+    public var label = ""
+    public var level = RulesEngineLogLevel.debug
+    public func log(level: RulesEngineLogLevel, label: String, message: String) {
+        self.message = message
+        self.level = level
+        self.label = label
+    }
+}
+
+class RulesEngineLoggingTests: XCTestCase {
+    func testLogging() {
+        let log = Log()
+        RulesEngineLog.logging = log
+        RulesEngineLog.debug(label: "labelA", "debug message")
+        XCTAssertEqual("labelA", log.label)
+        XCTAssertEqual("debug message", log.message)
+        XCTAssertEqual(RulesEngineLogLevel.debug, log.level)
+        RulesEngineLog.warning(label: "labelA", "warning message")
+        XCTAssertEqual(RulesEngineLogLevel.warning, log.level)
+        RulesEngineLog.error(label: "labelA", "error message")
+        XCTAssertEqual(RulesEngineLogLevel.error, log.level)
+    }
+}
