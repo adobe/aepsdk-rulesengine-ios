1b4558d2dc5b628e1f79f05c7497d6323d748908
Christopher Hoffmann <cdhoffmann@users.noreply.github.com>
2023-12-18 13:55:44 -0700

    prepare for major version bump

--- Sources/AEPRulesEngine/RulesEngine.swift
+++ Sources/AEPRulesEngine/RulesEngine.swift
@@ -15,7 +15,7 @@ import Foundation
 public typealias RulesTracer = (Bool, Rule, Context, RulesFailure?) -> Void
 
 public class RulesEngine<T: Rule> {
-    public let version = "4.0.0"
+    public let version = "5.0.0"
     let evaluator: Evaluating
     let transformer: Transforming
     var tracer: RulesTracer?
