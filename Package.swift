f0a2e36537995d99befbc3593daabd4705d5466a
Christopher Hoffmann <cdhoffmann@users.noreply.github.com>
2024-03-18 16:06:52 -0600

    add platform spec to package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AEPRulesEngine",
+    platforms: [.iOS(.v12), .tvOS(.v12)],
     products: [
         .library(name: "AEPRulesEngine", targets: ["AEPRulesEngine"]),
     ],
